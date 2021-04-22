90805d07bef0e9b60df06a9d97914f75980acc3f
Claire COMMISSAIRE <claire.commissaire@sciencespo.fr>
2021-04-22 12:05:43 +0200

    Adding redirection

--- srcs/launch.sh
+++ srcs/launch.sh
@@ -1,11 +1,11 @@
 if [ $autoindex != "off" ]
 then
 	export autoindex=on
-	sed -i '37d' /etc/nginx/sites-available/default 
+	sed -i '14d' /etc/nginx/sites-available/default
 fi
 if [ $autoindex = "off" ]
 then
-	sed -i '36d' /etc/nginx/sites-available/default
+	sed -i '13d' /etc/nginx/sites-available/default
 fi
 service php7.3-fpm start
 service nginx start
